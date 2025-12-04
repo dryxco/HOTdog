4dcad9e6232e155779695dd661d5f1d1db45350c
igotyabingo <jenifer7933@snu.ac.kr>
2025-12-04 18:23:15 +0900

    solved build problem with perception module

--- src/perception/setup.py
+++ src/perception/setup.py
@@ -11,15 +11,16 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/perception_launch.py']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'opencv-python'],
     zip_safe=True,
     maintainer='Suyeon Woo',
     maintainer_email='jenifer7933@snu.ac.kr',
     description='Perception package for object detection and localization',
     license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'perception_node = perception.nodes.perception_node:main',
+            'perception_node = perception.perception_node:main',
         ],
     },
 )
